f579557a563859a7aef322fa8a6965c4334ebeca
Vincent27_ <vincent.miras03@gmail.com>
2025-02-05 11:45:55 +0100

    routage de l'API avec des vérificaations des paramètres mis en URL

--- rainign/routes/live.js
+++ rainign/routes/live.js
@@ -4,12 +4,23 @@ var router = express.Router();
 /* GET home page. */
 router.get('/:list_capteur?', function(req, res, next) {
     const url = 'influxdb://localhost:8086'
-    const listCapteur = req.params.list_capteur;
+    const capteurs = req.params.list_capteur;
+    const valid_Capteur=['date','temperature','pressure','humidity','lux','wind_heading','wind_speed_avg','rain','lat','long']
 
-    if (!listCapteur) {
+    if (capteurs){
+        const listC=capteurs.split('-');
+        const listCapteur = Array.from(new Set(listC));
+        for (let element of listCapteur){
+            if (!valid_Capteur.includes(element)){
+                res.send(`Paramètres non valide`)
+            }
+        }
+        res.send(`Le paramètre listCapteur est : ${listCapteur}.`)
+    }
+
+    if (!capteurs) {
         return res.send('Le paramètre list_capteur est manquant, mais la route fonctionne !');
     }
-    res.send(`Le paramètre list_capteur est : ${listCapteur}`);
 });
 
 module.exports = router;
--- rainign/routes/sample.js
+++ rainign/routes/sample.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+function isValidUTC(string) {
+    const timestamp = Date.parse(string);
+    return !isNaN(timestamp) && string.endsWith('Z');
+  }
+
+
 /* GET home page. */
 router.get('/:start/:end/:list_capteur?', function(req, res, next) {
     const url = 'influxdb://localhost:8086'
     const startDate = req.params.start;
     const endDate = req.params.end;
-    const listCapteur = req.params.list_capteur;
+    const capteurs = req.params.list_capteur;
+    const valid_Capteur=['date','temperature','pressure','humidity','lux','wind_heading','wind_speed_avg','rain','lat','long']
 
     if (!startDate) {
         return res.send('Le paramètre startDate est manquant !');
@@ -14,9 +22,30 @@ router.get('/:start/:end/:list_capteur?', function(req, res, next) {
     if (!endDate) {
         return res.send('Le paramètre endDate est manquant !');
     }
+
+    if (!isValidUTC(startDate)){
+        res.send(`Date de début invalide`)
+    }
+
+    if (!isValidUTC(endDate)){
+        res.send(`Date de fin invalide`)
+    }
+
+    if (capteurs){
+        const listC=capteurs.split('-');
+        const listCapteur = Array.from(new Set(listC));
+        for (let element of listCapteur){
+            if (!valid_Capteur.includes(element)){
+                res.send(`Paramètres non valide`)
+            }
+        }
+        res.send(`Le paramètre startDate est : ${startDate}.
+            Le paramètre endDate est : ${endDate}.
+            Le paramètre listCapteur est : ${listCapteur}.`)
+    }
+
     res.send(`Le paramètre startDate est : ${startDate}.
-        Le paramètre endDate est : ${endDate}.
-        Le paramètre listCapteur est : ${listCapteur}.`)
+        Le paramètre endDate est : ${endDate}.`)
 
 });
 
