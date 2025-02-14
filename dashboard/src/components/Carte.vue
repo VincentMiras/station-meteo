2a6dfd1a45ae13795f8b7666bc9d4ec6a71e521d
Vincent27_ <vincent.miras03@gmail.com>
2025-02-14 16:05:13 +0100

    commit carte dashboard nul

---
+++ dashboard/src/components/Carte.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: {
+    type: Array,
+  },
+})
+</script>
+
+<template>
+  <div class="carto">
+    <h1 class="title">Positionnement de la station</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
