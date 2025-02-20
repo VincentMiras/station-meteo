4fc0647c6a8c4f2d5ac579c95857447f7c4ac2a3
ibrahim-sall <ibhou2002@gmail.com>
2025-02-20 22:53:22 +0100

    add tph survey, ready to merge

--- db/db.js
+++ db/db.js
@@ -6,6 +6,7 @@ import nmea from 'nmea-simple';
 const gpsFilePath = '/dev/shm/gpsNmea';
 const rainFilePath = '/dev/shm/rainCounter.log';
 const sensorFilePath = '/dev/shm/sensors';
+const tphFilePath = '/dev/shm/tph.log';
 
 const token = fs.readFileSync(process.env.DOCKER_INFLUXDB_INIT_ADMIN_TOKEN_FILE, 'utf8').trim();
 
@@ -115,12 +116,14 @@ fs.watchFile(sensorFilePath, () => {
                 const data = JSON.parse(line);
                 const timestamp = new Date(data.date);
                 data.measure.forEach(measurement => {
-                    const point = new Point(measurement.name)
-                        .timestamp(timestamp)
-                        .floatField('value', parseFloat(measurement.value))
-                        .tag('unit', measurement.unit);
-                    writeClient.writePoint(point);
-                    // console.log(`Sensor Data written to DB: ${JSON.stringify(measurement)}`);
+                    if (!["temperature", "pressure", "humidity"].includes(measurement.name)) {
+                        const point = new Point(measurement.name)
+                            .timestamp(timestamp)
+                            .floatField('value', parseFloat(measurement.value))
+                            .tag('unit', measurement.unit);
+                        writeClient.writePoint(point);
+                        console.log(`Sensor Data written to DB: ${JSON.stringify(measurement)}`);
+                    }
                 });
             } catch (error) {
                 console.error("Got bad packet:", line, error);
@@ -128,5 +131,45 @@ fs.watchFile(sensorFilePath, () => {
             }
         });
         fileLineCount.sensor = linesNumber;
+    }
+});
+
+fs.watchFile(tphFilePath, () => {
+    const lines = fs.readFileSync(tphFilePath, 'utf8').split('\n');
+    const linesNumber = lines.length;
+    if (linesNumber < fileLineCount.tph) {
+        fileLineCount.tph = 0;
+    }
+
+    if (linesNumber > fileLineCount.tph) {
+        const newLines = lines.slice(fileLineCount.tph, linesNumber);
+        console.log(fileLineCount.tph, linesNumber, newLines);
+        newLines.forEach(line => {
+            try {
+                line = line.trim();
+                const data = JSON.parse(line);
+                const timestamp = new Date(data.date);
+                const tempPoint = new Point('temperature')
+                    .timestamp(timestamp)
+                    .floatField('value', data.temp)
+                    .tag('unit', 'C');
+                const hygroPoint = new Point('humidity')
+                    .timestamp(timestamp)
+                    .floatField('value', data.hygro)
+                    .tag('unit', '%');
+                const pressPoint = new Point('pressure')
+                    .timestamp(timestamp)
+                    .floatField('value', data.press)
+                    .tag('unit', 'hPa');
+                writeClient.writePoint(tempPoint);
+                writeClient.writePoint(hygroPoint);
+                writeClient.writePoint(pressPoint);
+                console.log(`TPH Data written to DB: ${JSON.stringify(data)}`);
+            } catch (error) {
+                console.error("Got bad packet:", line, error);
+                console.log(`TPH Data not written to DB: ${line}`);
+            }
+        });
+        fileLineCount.tph = linesNumber;
     }
 });
