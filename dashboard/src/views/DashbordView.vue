422856951454e6c5413e9318594da6c99036635f
VincentMiras <vincent.miras03@gmail.com>
2025-02-23 14:19:53 +0100

    Petit ajustement archi code

--- dashboard/src/views/DashbordView.vue
+++ dashboard/src/views/DashbordView.vue
@@ -1,7 +1,9 @@
 <script setup>
-import Carte from '@/components/Carte.vue';
-import Temperature from '@/components/Temperature.vue';
-import Pression from '@/components/Pression.vue';
+import Carte from '@/components/mesure/Carte.vue';
+import Temperature from '@/components/mesure/Temperature.vue';
+import Pression from '@/components/mesure/Pression.vue';
+import Humidité from '@/components/mesure/Humidite.vue';
+
 
 import { useWeatherStore } from '@/stores/weatherStore';
 
@@ -10,6 +12,7 @@ const weatherStore = useWeatherStore();
 </script>
 
 <template>
+    <Humidité :humi="21"/>
     <Pression :press="21"/>
     <Temperature :temp="21"/>
     <Carte :coords="[[41,1]]"/>
