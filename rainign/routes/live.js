f916f38b3f94121158c5650714c6acfe09fe7cdc
ibrahim-sall <ibhou2002@gmail.com>
2025-02-26 13:11:39 +0100

    add decimal rounding to live route

--- rainign/routes/live.js
+++ rainign/routes/live.js
@@ -1,9 +1,7 @@
 var fs = require('fs');
-
 var express = require('express');
 const { InfluxDB } = require('@influxdata/influxdb-client');
 var router = express.Router();
-
 
 function sendError(res) {
     return res.status(400).json({
@@ -12,7 +10,6 @@ function sendError(res) {
 }
 
 const url = process.env.INFLUXDB_URL || 'http://piensg031.ensg.eu:8086';
-
 const token = fs.readFileSync(process.env.DOCKER_INFLUXDB_INIT_ADMIN_TOKEN_FILE, 'utf8').trim();
 const org = process.env.DOCKER_INFLUXDB_INIT_ORG || 'docs';
 const bucket = process.env.DOCKER_INFLUXDB_INIT_BUCKET || 'meteo';
@@ -21,7 +18,6 @@ const client = new InfluxDB({ url, token });
 const queryApi = client.getQueryApi(org);
 
 const valid_Capteur = ['temperature', 'pressure', 'humidity', 'lux', 'wind_heading', 'wind_speed_avg', 'rain', 'lat', 'lon'];
-
 
 const capteurMapping = {
     temperature: 'temperature',
@@ -47,7 +43,17 @@ const unitMapping = {
     lon: 'DD'
 };
 
-
+const decimalPlaces = {
+    temperature: 2,
+    pressure: 2,
+    humidity: 2,
+    rain: 2,
+    lux: 2,
+    wind_heading: 0,
+    wind_speed_avg: 0,
+    lat: 2,
+    lon: 3
+};
 
 router.get('/:list_capteur?', function (req, res, next) {
     let capteurs = req.params.list_capteur;
@@ -75,7 +81,7 @@ router.get('/:list_capteur?', function (req, res, next) {
         }
         try {
             const data = await queryApi.collectRows(query);
-            return data.length > 0 ? data[0]._value : null;
+            return data.length > 0 ? parseFloat(data[0]._value.toFixed(decimalPlaces[capteur])) : null;
         } catch (error) {
             console.error(`Error fetching data for ${capteur}:`, error);
             return null;
